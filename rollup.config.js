4fefbd2e02247a7266eba8bd9986d00cd44a5cb3
Hao Zhang <hzhangxyz@outlook.com>
2025-12-12 12:31:20 +0800

    Init commit.

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
+
+export default {
+    input: "main.ts",
+    output: {
+        file: "dist/main.js",
+        format: "es",
+        banner: "#!/usr/bin/env node",
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        json(),
+        typescript(),
+        terser()
+    ]
+};
